85b5183d3264ba4e1c43af7f025aecc063cf260f
SLiX69 <asd2304@gmail.com>
2016-04-06 15:22:53 +0200

    code reformat

--- addon.py
+++ addon.py
@@ -1,22 +1,23 @@
-import re,xbmcplugin,xbmcgui,requests
+import re, xbmcplugin, xbmcgui, requests
 import xbmc
 import xbmcaddon
 import os
 import urlparse
-from urllib import quote, unquote_plus, unquote, urlencode,quote_plus,urlretrieve
+from urllib import quote, unquote_plus, unquote, urlencode, quote_plus, urlretrieve
 from resources import pafy
 
-pluginhandle=int(sys.argv[1])
-addon=xbmcaddon.Addon(id='plugin.video.doku.cc')
-home=addon.getAddonInfo('path').decode('utf-8')
-icon=xbmc.translatePath(os.path.join(home, 'icon.png'))
-fanart=xbmc.translatePath(os.path.join(home, 'fanart.jpg'))
-imageDir=os.path.join(home, 'thumbnails') + '/'
-view_mode_id=int('503')
-xbox=xbmc.getCondVisibility("System.Platform.xbox")
-downloadpath=addon.getSetting('download_path')
+pluginhandle = int(sys.argv[1])
+addon = xbmcaddon.Addon(id='plugin.video.doku.cc')
+home = addon.getAddonInfo('path').decode('utf-8')
+icon = xbmc.translatePath(os.path.join(home, 'icon.png'))
+fanart = xbmc.translatePath(os.path.join(home, 'fanart.jpg'))
+imageDir = os.path.join(home, 'thumbnails') + '/'
+view_mode_id = int('503')
+xbox = xbmc.getCondVisibility("System.Platform.xbox")
+downloadpath = addon.getSetting('download_path')
 xbmcplugin.setContent(pluginhandle, 'Episodes')
-baseurl='http://doku.cc//api.php?'
+baseurl = 'http://doku.cc//api.php?'
+
 
 def categories():
     genres = get_genres()
@@ -24,133 +25,156 @@ def categories():
         url = genre['url']
         name = genre['genre']
         icon = genre['thumb']
-        addDir(name,url,'index',icon)
-    addDir('Suche','','Search',imageDir+'6.png')
-    addDir('Kategorien','','getcat',imageDir+'7.png')
-    addDir('A-Z','','Alphabet',imageDir+'8.png')
+        addDir(name, url, 'index', icon)
+    addDir('Suche', '', 'Search', imageDir + '6.png')
+    addDir('Kategorien', '', 'getcat', imageDir + '7.png')
+    addDir('A-Z', '', 'Alphabet', imageDir + '8.png')
     if downloadpath is '':
         pass
     else:
-        addDir('meine Downloads','','listing',imageDir+'9.png')
+        addDir('meine Downloads', '', 'listing', imageDir + '9.png')
+
 
 def get_genres():
-    genres = ({'url': '%sget=new-dokus&page=1' % baseurl, 'genre': 'Die neusten Dokus', 'thumb': imageDir+'1.png'}, {'url': '%sget=reuploads&page=1' % baseurl, 'genre': 'Die neusten reUploads', 'thumb': imageDir+'2.png'}, {'url': '%stop-dokus=trend&page=1' % baseurl, 'genre': 'Aufsteiger der Woche', 'thumb': imageDir+'3.png'},  {'url': '%stop-dokus=last-month&page=1' % baseurl, 'genre': 'Top Dokus des Monats', 'thumb': imageDir+'4.png'}, {'url': '%stop-dokus=last-year&page=1' % baseurl, 'genre': 'Top Dokus des Jahres', 'thumb': imageDir+'5.png'})
+    genres = ({'url': '%sget=new-dokus&page=1' % baseurl, 'genre': 'Die neusten Dokus', 'thumb': imageDir + '1.png'},
+              {'url': '%sget=reuploads&page=1' % baseurl, 'genre': 'Die neusten reUploads',
+               'thumb': imageDir + '2.png'},
+              {'url': '%stop-dokus=trend&page=1' % baseurl, 'genre': 'Aufsteiger der Woche',
+               'thumb': imageDir + '3.png'},
+              {'url': '%stop-dokus=last-month&page=1' % baseurl, 'genre': 'Top Dokus des Monats',
+               'thumb': imageDir + '4.png'},
+              {'url': '%stop-dokus=last-year&page=1' % baseurl, 'genre': 'Top Dokus des Jahres',
+               'thumb': imageDir + '5.png'})
     return genres
 
+
 def getcat():
-    url='%sgetCats' % baseurl
-    data=getjson(url)
+    url = '%sgetCats' % baseurl
+    data = getjson(url)
     for item in data:
-        name=item['name']
-        url=item['url']
-        addDir(name,url,'index',icon)
+        name = item['name']
+        url = item['url']
+        addDir(name, url, 'index', icon)
+
 
 def index(url):
-    data=getjson(url)
+    data = getjson(url)
     for item in data['dokus']:
-        url=item['youtubeId']
-        desc=item['description']
-        name=item['title']
-        thumb=item['cover']
-        duration=item['length']
-        date=cleandate(item['date'])
-        desc='%s       %s %s  bei  %s   Votes\n%s' % (date, item['voting']['voteCountInPerc'],'%', item['voting']['voteCountAll'],item['description'])
-        addLink(name,url,'play',thumb,desc,duration)
+        url = item['youtubeId']
+        desc = item['description']
+        name = item['title']
+        thumb = item['cover']
+        duration = item['length']
+        date = cleandate(item['date'])
+        desc = '%s       %s %s  bei  %s   Votes\n%s' % (
+        date, item['voting']['voteCountInPerc'], '%', item['voting']['voteCountAll'], item['description'])
+        addLink(name, url, 'play', thumb, desc, duration)
     try:
-        url=(data['query']['nextpage'])
-        addDir('Next',url,'index',imageDir+'10.png')
+        url = (data['query']['nextpage'])
+        addDir('Next', url, 'index', imageDir + '10.png')
     except:
         pass
     try:
-        url=(data['query']['prevpage'])
-        addDir('Prev',url,'index',imageDir+'11.png')
+        url = (data['query']['prevpage'])
+        addDir('Prev', url, 'index', imageDir + '11.png')
     except:
         pass
 
-        
+
 def play(url):
-    video_url=((pafy.new(url)).getbest()).url
-    listitem=xbmcgui.ListItem(path=video_url)
+    video_url = ((pafy.new(url)).getbest()).url
+    listitem = xbmcgui.ListItem(path=video_url)
     xbmcplugin.setResolvedUrl(pluginhandle, succeeded=True, listitem=listitem)
 
+
 def Search():
-    search_entered=search()
-    url='%ssearch=%s&page=1' % (baseurl,search_entered)
+    search_entered = search()
+    url = '%ssearch=%s&page=1' % (baseurl, search_entered)
     index(url)
 
+
 def search():
-        search_entered=''
-        keyboard=xbmc.Keyboard(search_entered, 'Suche auf Doku.cc')
-        keyboard.doModal()
-        if keyboard.isConfirmed():
-            search_entered=keyboard.getText()
-            if search_entered == None:
-                return False          
-        return search_entered 
-              
+    search_entered = ''
+    keyboard = xbmc.Keyboard(search_entered, 'Suche auf Doku.cc')
+    keyboard.doModal()
+    if keyboard.isConfirmed():
+        search_entered = keyboard.getText()
+        if search_entered == None:
+            return False
+    return search_entered
+
+
 def Alphabet():
-        for i in range(ord('A'), ord('Z')+1):
-            name=chr(i)
-            url='%sletter=%s&page=1' % (baseurl,name)
-            addDir(name,url,'index',icon)
+    for i in range(ord('A'), ord('Z') + 1):
+        name = chr(i)
+        url = '%sletter=%s&page=1' % (baseurl, name)
+        addDir(name, url, 'index', icon)
+
 
 def Download(url):
     if downloadpath is '':
-        d=xbmcgui.Dialog()
-        d.ok('Download Error','Du hast keinen Download Folder gesetzt','','')
-        addon.openSettings(sys.argv[ 0 ])
+        d = xbmcgui.Dialog()
+        d.ok('Download Error', 'Du hast keinen Download Folder gesetzt', '', '')
+        addon.openSettings(sys.argv[0])
         return
     xbmc.executebuiltin('XBMC.Notification(Doku.cc,Starte Download)')
-    name=(pafy.new(url)).title
-    best=(pafy.new(url)).getbest()
-    filepath=downloadpath+name+'.'+best.extension
-    filepath=(filepath).replace('.temp','')
-    best.download(filepath).replace('.temp','')
+    name = (pafy.new(url)).title
+    best = (pafy.new(url)).getbest()
+    filepath = downloadpath + name + '.' + best.extension
+    filepath = (filepath).replace('.temp', '')
+    best.download(filepath).replace('.temp', '')
     xbmc.executebuiltin("XBMC.Notification(Download beendet!,2000)")
 
+
 def listing():
-    dirs=os.listdir(downloadpath)
+    dirs = os.listdir(downloadpath)
     for url in dirs:
-        url=downloadpath+url
-        name=(url).replace(downloadpath,'')
-        addLink(name,url,'play1',icon,'','')
+        url = downloadpath + url
+        name = (url).replace(downloadpath, '')
+        addLink(name, url, 'play1', icon, '', '')
+
 
 def play1(url):
-    videolink=str(url)
-    listitem=xbmcgui.ListItem(path=videolink)
+    videolink = str(url)
+    listitem = xbmcgui.ListItem(path=videolink)
     xbmcplugin.setResolvedUrl(pluginhandle, succeeded=True, listitem=listitem)
 
+
 def cleandate(date):
-    date=date.split(' ', 1)[0]
-    date='%s.%s.%s' % (date.split('-')[2],date.split('-')[1],date.split('-')[0])
+    date = date.split(' ', 1)[0]
+    date = '%s.%s.%s' % (date.split('-')[2], date.split('-')[1], date.split('-')[0])
     return date
 
+
 def getjson(url):
-    r=requests.get(url)
-    data=r.json()
+    r = requests.get(url)
+    data = r.json()
     r.connection.close()
     return data
-        
-def addLink(name,url,mode,iconimage,desc,duration):
-    u=sys.argv[0]+"?url="+quote_plus(url)+"&mode="+str(mode)
-    ok=True
-    item=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
-    item.setInfo( type="Video", infoLabels={ 'Genre': 'Doku', "Title": name , "Plot": desc , "Duration" : duration } )
+
+
+def addLink(name, url, mode, iconimage, desc, duration):
+    u = sys.argv[0] + "?url=" + quote_plus(url) + "&mode=" + str(mode)
+    ok = True
+    item = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+    item.setInfo(type="Video", infoLabels={'Genre': 'Doku', "Title": name, "Plot": desc, "Duration": duration})
     item.setProperty('IsPlayable', 'true')
     menu = []
     menu.append(('Download Video', 'XBMC.RunPlugin(%s?mode=59&name=%s&url=%s)' % (sys.argv[0], name, url)))
     item.addContextMenuItems(items=menu, replaceItems=False)
     item.setProperty('fanart_image', fanart)
     xbmc.executebuiltin('Container.SetViewMode(%d)' % view_mode_id)
-    xbmcplugin.addDirectoryItem(pluginhandle,url=u,listitem=item)
+    xbmcplugin.addDirectoryItem(pluginhandle, url=u, listitem=item)
 
-def addDir(name,url,mode,iconimage):
-    u=sys.argv[0]+"?url="+quote_plus(url)+"&mode="+str(mode)+"&name="+quote_plus(name)
-    ok=True
-    item=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-    item.setInfo( type="Video", infoLabels={ "Title": name } )
+
+def addDir(name, url, mode, iconimage):
+    u = sys.argv[0] + "?url=" + quote_plus(url) + "&mode=" + str(mode) + "&name=" + quote_plus(name)
+    ok = True
+    item = xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+    item.setInfo(type="Video", infoLabels={"Title": name})
     item.setProperty('fanart_image', fanart)
-    xbmcplugin.addDirectoryItem(pluginhandle,url=u,listitem=item,isFolder=True)	
+    xbmcplugin.addDirectoryItem(pluginhandle, url=u, listitem=item, isFolder=True)
+
 
 def parameters_string_to_dict(parameters):
     ''' Convert parameters encoded in a URL to a dict. '''
@@ -163,29 +187,30 @@ def parameters_string_to_dict(parameters):
                 paramDict[paramSplits[0]] = paramSplits[1]
     return paramDict
 
-params=parameters_string_to_dict(sys.argv[2])
-mode=params.get('mode')
-url=params.get('url')
-if type(url)==type(str()):
-  url=unquote_plus(url)
+
+params = parameters_string_to_dict(sys.argv[2])
+mode = params.get('mode')
+url = params.get('url')
+if type(url) == type(str()):
+    url = unquote_plus(url)
 
 if mode == 'index':
     index(url)
-elif mode =='play':
+elif mode == 'play':
     play(url)
-elif mode =='Search':	
+elif mode == 'Search':
     Search()
-elif mode =='Alphabet':
+elif mode == 'Alphabet':
     Alphabet()
-elif mode =='getcat':
+elif mode == 'getcat':
     getcat()
-elif mode =='listing':
+elif mode == 'listing':
     listing()
-elif mode =='play1':
+elif mode == 'play1':
     play1(url)
-elif mode =='59':
+elif mode == '59':
     Download(url)
 else:
-   categories()
+    categories()
 
 xbmcplugin.endOfDirectory(pluginhandle)
