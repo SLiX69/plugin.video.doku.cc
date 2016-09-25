0b6ededb441ae0c25d9b4130d7db0e5acfb70786
SLiX69 <asd2304@gmail.com>
2016-09-25 20:37:34 +0200

    added translations + small cleanup

--- addon.py
+++ addon.py
@@ -14,6 +14,8 @@ icon = xbmc.translatePath(os.path.join(home, 'icon.png'))
 fanart = xbmc.translatePath(os.path.join(home, 'fanart.jpg'))
 imageDir = os.path.join(home, 'thumbnails') + '/'
 view_mode_id = int('503')
+TRANSLATE = addon.getLocalizedString
+
 show_doku_src = addon.getSetting('show_doku_source')
 show_menu_search = addon.getSetting('show_menu_search')
 show_menu_cats = addon.getSetting('show_menu_cats')
@@ -37,11 +39,11 @@ if addon.getSetting('change_view') == 'true':
     view_mode_id = int(addon.getSetting('change_view_episodes'))
 
 dis_genre = []
-if addon.getSetting('show_menu_new') == 'false': dis_genre.append('Die neusten Dokus')
-if addon.getSetting('show_menu_reup') == 'false': dis_genre.append('Die neusten reUploads')
-if addon.getSetting('show_menu_week') == 'false': dis_genre.append('Aufsteiger der Woche')
-if addon.getSetting('show_menu_month') == 'false': dis_genre.append('Top Dokus des Monats')
-if addon.getSetting('show_menu_year') == 'false': dis_genre.append('Top Dokus des Jahres')
+if addon.getSetting('show_menu_new') == 'false': dis_genre.append(TRANSLATE(30010))
+if addon.getSetting('show_menu_reup') == 'false': dis_genre.append(TRANSLATE(30011))
+if addon.getSetting('show_menu_week') == 'false': dis_genre.append(TRANSLATE(30012))
+if addon.getSetting('show_menu_month') == 'false': dis_genre.append(TRANSLATE(30013))
+if addon.getSetting('show_menu_year') == 'false': dis_genre.append(TRANSLATE(30014))
 
 sett_desc_show_date = False
 sett_desc_show_vote = False
@@ -60,31 +62,31 @@ def categories():
         if name in dis_genre:
             continue
         addDir(name, url, 'index', icon)
-    if show_menu_search == 'true': addDir('Suche', '', 'Search', imageDir + '6.png')
-    if show_menu_cats == 'true': addDir('Kategorien', '', 'getcat', imageDir + '7.png')
-    if show_menu_abc == 'true': addDir('A-Z', '', 'Alphabet', imageDir + '8.png')
-    if script_chk('plugin.video.bookmark') == 1: addDir('Merkliste', '', 'merk', imageDir + '9.png')
+    if show_menu_search == 'true': addDir(TRANSLATE(30015), '', 'Search', imageDir + '6.png')
+    if show_menu_cats == 'true': addDir(TRANSLATE(30016), '', 'getcat', imageDir + '7.png')
+    if show_menu_abc == 'true': addDir(TRANSLATE(30017), '', 'Alphabet', imageDir + '8.png')
+    if script_chk('plugin.video.bookmark') == 1: addDir(TRANSLATE(30018), '', 'merk', imageDir + '9.png')
     xbmcplugin.endOfDirectory(pluginhandle)
     if change_view:
         xbmc.executebuiltin('Container.SetViewMode(%d)' % view_mode_id)
 
 
 def get_genres():
-    genres = ({'url': '%sget=new-dokus&page=1' % baseurl, 'genre': 'Die neusten Dokus', 'thumb': imageDir + '1.png'},
-              {'url': '%sget=reuploads&page=1' % baseurl, 'genre': 'Die neusten reUploads',
+    genres = ({'url': '%sget=new-dokus&page=1' % baseurl, 'genre': TRANSLATE(30010), 'thumb': imageDir + '1.png'},
+              {'url': '%sget=reuploads&page=1' % baseurl, 'genre': TRANSLATE(30011),
                'thumb': imageDir + '2.png'},
-              {'url': '%stop-dokus=trend&page=1' % baseurl, 'genre': 'Aufsteiger der Woche',
+              {'url': '%stop-dokus=trend&page=1' % baseurl, 'genre': TRANSLATE(30012),
                'thumb': imageDir + '3.png'},
-              {'url': '%stop-dokus=last-month&page=1' % baseurl, 'genre': 'Top Dokus des Monats',
+              {'url': '%stop-dokus=last-month&page=1' % baseurl, 'genre': TRANSLATE(30013),
                'thumb': imageDir + '4.png'},
-              {'url': '%stop-dokus=last-year&page=1' % baseurl, 'genre': 'Top Dokus des Jahres',
+              {'url': '%stop-dokus=last-year&page=1' % baseurl, 'genre': TRANSLATE(30014),
                'thumb': imageDir + '5.png'})
     return genres
 
 
-def getcat():
+def get_cat():
     url = '%sgetCats' % baseurl
-    data = getjson(url)
+    data = get_json(url)
     for item in data:
         name = item['name']
         url = item['url']
@@ -95,7 +97,7 @@ def getcat():
 
 
 def index(url):
-    data = getjson(url)
+    data = get_json(url)
     for item in data['dokus']:
         url = item['youtubeId']
         desc = item['description']
@@ -104,21 +106,21 @@ def index(url):
         #thumb = 'http://img.youtube.com/vi/' + url + '/0.jpg'
         fanart = get_fanart(url)
         duration = item['length']
-        date = cleandate(item['date'])
+        date = clean_date(item['date'])
         source = get_item_src(item['dokuSrc'])
         perc = get_item_perc(item['voting']['voteCountInPerc'])
         vote = get_item_vote(item['voting']['voteCountAll'])
-        desc = getdesc(date, perc, vote, source, desc)
+        desc = get_desc(date, perc, vote, source, desc)
         addLink(name, url, 'play', thumb, desc, duration, date, fanart)
     try:
         url = (data['query']['nextpage'])
-        addDir('Next', url, 'index', imageDir + '10.png')
+        addDir(TRANSLATE(30030), url, 'index', imageDir + '10.png')
     except:
         pass
     try:
         url = (data['query']['prevpage'])
-        addDir('Prev', url, 'index', imageDir + '11.png')
-        if show_mm: addDir('Hauptmenü', '', '', imageDir + '12.png')
+        addDir(TRANSLATE(30031), url, 'index', imageDir + '11.png')
+        if show_mm: addDir(TRANSLATE(30032), '', '', imageDir + '12.png')
     except:
         pass
     if change_view:
@@ -126,21 +128,15 @@ def index(url):
     xbmcplugin.endOfDirectory(pluginhandle)
 
 
-def play(url):
-    video_url = "plugin://plugin.video.youtube/play/?video_id="+url
-    listitem = xbmcgui.ListItem(path=video_url)
-    xbmcplugin.setResolvedUrl(pluginhandle, succeeded=True, listitem=listitem)
-
-
-def Search():
-    search_entered = search()
+def search():
+    search_entered = search_dialog()
     url = '%ssearch=%s&page=1' % (baseurl, search_entered)
     index(url)
 
 
-def search():
+def search_dialog():
     search_entered = ''
-    keyboard = xbmc.Keyboard(search_entered, 'Suche auf %s' % title)
+    keyboard = xbmc.Keyboard(search_entered, TRANSLATE(30040))
     keyboard.doModal()
     if keyboard.isConfirmed():
         search_entered = keyboard.getText()
@@ -149,7 +145,7 @@ def search():
     return search_entered
 
 
-def Alphabet():
+def list_alphabet():
     for i in range(ord('A'), ord('Z') + 1):
         name = chr(i)
         url = '%sletter=%s&page=1' % (baseurl, name)
@@ -159,7 +155,7 @@ def Alphabet():
         xbmc.executebuiltin('Container.SetViewMode(%d)' % view_mode_id)
 
 
-def cleandate(date):
+def clean_date(date):
     date = date.split(' ', 1)[0]
     date = '%s.%s.%s' % (date.split('-')[2], date.split('-')[1], date.split('-')[0])
     return date
@@ -170,7 +166,7 @@ def get_item_src(source):
         if source.upper() != 'PROGRAMM' and len(source) > 2:
             if len(source) > 15:
                 source = source[0:14]
-            source = 'von: ' + source
+            source = TRANSLATE(30043) + source
         else:
             source = ''
     else:
@@ -198,14 +194,14 @@ def get_item_vote(vote):
     return vote
 
 
-def getjson(url):
+def get_json(url):
     r = requests.get(url)
     data = r.json()
     r.connection.close()
     return data
 
 
-def getdesc(date, perc, vote, source, description):
+def get_desc(date, perc, vote, source, description):
     desc = ''
     if sett_desc_show_date: desc = date + '   '
     if sett_desc_show_vote: desc += vote + '  ' + perc + '   '
@@ -236,6 +232,12 @@ def exists(path):
 
 def script_chk(script_name):
     return xbmc.getCondVisibility('System.HasAddon(%s)' % script_name) == 1
+
+
+def play(url):
+    video_url = "plugin://plugin.video.youtube/play/?video_id="+url
+    listitem = xbmcgui.ListItem(path=video_url)
+    xbmcplugin.setResolvedUrl(pluginhandle, succeeded=True, listitem=listitem)
 
 
 def addLink(name, url, mode, iconimage, desc, duration, date, fanart):
@@ -284,15 +286,12 @@ if mode == 'index':
 elif mode == 'play':
     play(url)
 elif mode == 'Search':
-    Search()
+    search()
 elif mode == 'Alphabet':
-    Alphabet()
+    list_alphabet()
 elif mode == 'getcat':
-    getcat()
+    get_cat()
 elif mode == 'merk':
-    xbmc.log('MERK')
-    xbmc.executebuiltin(
-        "XBMC.RunPlugin(\"plugin://plugin.video.bookmark/?mode=episodes&url=plugin.video.doku5.com\")")
     xbmc.executebuiltin("ActivateWindow(10024,plugin://plugin.video.bookmark/?mode=episodes&url=plugin.video.doku5.com)")
 else:
     categories()
